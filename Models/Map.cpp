2d3967eba2b12b2272829ad24e9b4a85bf5ae545
meoril <meorilehr@gmail.com>
2016-05-31 18:59:03 +0300

    fix map inflate and conversion to grid

--- Models/Map.cpp
+++ Models/Map.cpp
@@ -9,120 +9,129 @@
 using namespace std;
 
 Map::Map(const char* filename) {
-	initMap(filename);
+    initMap(filename);
 }
 
 Map::Map(unsigned Cols, unsigned Rows) {
-	this->m_Cols = Cols;
-	this->m_Rows = Rows;
-	this->FatGrid.resize(Rows, Cols);
-	this->RegGrid.resize(Rows, Cols);
+    this->m_Cols = Cols;
+    this->m_Rows = Rows;
 }
 
 void Map::initMap(const char* filename) {
-	std::vector<unsigned char> image;
-	std::vector<unsigned char> FatImage;
-	unsigned width, height;
-	unsigned x, y, bX, bY;
-
-	// Get the config
-	ConfigurationManager* config = ConfigurationManager::GetInstance();
-
-	//decode
-	unsigned error = lodepng::decode(image, width, height,
-			config->getPngMapPath());
-
-	//if there's an error, display it
-	if (error)
-		std::cout << "decoder error " << error << ": "
-				<< lodepng_error_text(error) << std::endl;
-
-	// Create the fat img
-	FatImage.resize(width * height);
-
-	// calc the size of the robot in pic px
-	unsigned PxToBlow = ceil(
-			config->getRobotSize().RadiosSize()
-					/ config->getPngGridResolution());
-
-	// run on the map and find the
-	unsigned char color;
-	for (y = 0; y < height; y++)
-		for (x = 0; x < width; x++) {
-			if (image[y * width * 4 + x * 4 + 0]
-					|| image[y * width * 4 + x * 4 + 1]
-					|| image[y * width * 4 + x * 4 + 2])
-				color = 0;
-
-			// add oppstical to the fat img
-			for (bX = std::max(x - PxToBlow, static_cast<unsigned int>(0));
-					bX < PxToBlow + x; bX++)
-				for (bY = std::max(x - PxToBlow, static_cast<unsigned int>(0));
-						bY < PxToBlow + y; bY++) {
-					FatImage[y * width * 4 + x * 4 + 0] = color;
-					FatImage[y * width * 4 + x * 4 + 1] = color;
-					FatImage[y * width * 4 + x * 4 + 2] = color;
-					FatImage[y * width * 4 + x * 4 + 3] = 255;
-				}
-		}
-
-	// create grid from the fat and regular map
-
-	this->FatGrid = this->CreatGridFromMap(FatImage, height, width,
-			config->getPngGridResolution(), config->getPixelPerCm(),
-			this->m_Cols, this->m_Rows);
-
-
-	unsigned error2 = lodepng::encode("try.png", this->FatGrid, width, height);
-
-	this->RegGrid = this->CreatGridFromMap(image, height, width,
-			config->getPngGridResolution(), config->getPixelPerCm(),
-			this->m_Cols, this->m_Rows);
-
+    unsigned x, y, bX, bY;
+    
+    // Get the config
+    ConfigurationManager* config = new ConfigurationManager(filename);
+    
+    //decode
+    unsigned error = lodepng::decode(RegImage, width, height,
+                                     config->getMapPath());
+    
+    //if there's an error, display it
+    if (error)
+        std::cout << "decoder error " << error << ": "
+        << lodepng_error_text(error) << std::endl;
+    
+    // Create the fat img
+    FatImage.resize(width * height * 4);
+    
+    // calc the size of the robot in pic px
+    unsigned PxToBlow = ceil(
+                             config->getRobotSize().RadiosSize()
+                             / config->getPngGridResolution());
+    
+    cout <<RegImage.size();
+    // Paint the new map image in white
+    for(unsigned int i = 0; i < RegImage.size(); i++)
+    {
+        FatImage[i] = RegImage[i];
+    }
+    
+    
+    for (unsigned int i = 0; i < height; i++)
+        for (unsigned int j = 0; j < width; j++) {
+            if ((RegImage[i * width * 4 + j * 4 + 0] == 0) &&
+                (RegImage[i * width * 4 + j * 4 + 1] == 0) &&
+                (RegImage[i * width * 4 + j * 4 + 2] == 0))
+            {
+                
+                for(unsigned int x =i-PxToBlow; x<=i+PxToBlow;x++)
+                {
+                    // check if still in x axis bounding
+                    if (x >= 0 && x < height)
+                    {
+                        for (unsigned int y=j-PxToBlow; y<=j+PxToBlow; y++)
+                        {
+                            // check if still in y axis bounding
+                            if (y >= 0 && y < width)
+                            {
+                                FatImage[x * width * 4 + y * 4 + 0] = 0;
+                                FatImage[x * width * 4 + y * 4 + 1] = 0;
+                                FatImage[x * width * 4 + y * 4 + 2] = 0;
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+    
+    // create grid from the fat and regular map
+    this->FatGrid = this->CreatGridFromMap(FatImage, height, width,
+                                           config->getPngGridResolution(), config->getPixelPerCm(),
+                                           this->m_Cols, this->m_Rows);
+    
+    
+    
+    this->RegGrid = this->CreatGridFromMap(RegImage, height, width,
+                                           config->getPngGridResolution(), config->getPixelPerCm(),
+                                           this->m_Cols, this->m_Rows);
+    
 }
 
-std::vector<unsigned char> Map::CreatGridFromMap(std::vector<unsigned char> PngMap,
-		unsigned MapHeight, unsigned MapWidth, float GridResolutionCm,
-		float PixelPerCm, unsigned& GridCols, unsigned& GridRows) {
-
-	// Calc grid size
-	unsigned GridCellSizeInPx = ceil(GridResolutionCm / PixelPerCm);
-	GridCols = ceil(MapWidth * PixelPerCm / GridResolutionCm);
-	GridRows = ceil(MapHeight * PixelPerCm / GridResolutionCm);
-
-	//crate the grid
-	std::vector<unsigned char> Grid;
-	Grid.resize(GridCols, GridRows);
-
-	for (unsigned i = 0; i < GridCols; ++i) {
-		for (unsigned j = 0; j < GridRows; ++j) {
-
-			// defind white px counter
-			unsigned PxWhiteConuter = 0;
-
-			// find out from the png if cell is black or white
-			for (unsigned pI = 0; pI < GridCellSizeInPx; ++pI) {
-				for (unsigned pJ = 0; pJ < GridCellSizeInPx; ++pJ) {
-					PxWhiteConuter += PngMap[(i * GridCellSizeInPx * MapWidth
-							+ pI) * 4 + (j * GridCellSizeInPx + pJ) * 4];
-				}
-			}
-
-			// Chack for number of black cell TODO:think if i need a parameter for Negligible number of blac px
-			if (PxWhiteConuter == (GridCellSizeInPx * GridCellSizeInPx)) {
-				// white
-				Grid[i * GridCols + j] = 1;
-			} else {
-				// Black
-				Grid[i * GridCols + j] = 0;
-			}
-
-		}
-
-	}
-
-	return Grid;
-
+std::vector< std::vector<unsigned char> > Map::CreatGridFromMap(std::vector<unsigned char> PngMap,
+                                                                unsigned MapHeight, unsigned MapWidth, float GridResolutionCm,
+                                                                float PixelPerCm, unsigned& GridCols, unsigned& GridRows) {
+    
+    // Calc grid size
+    unsigned GridCellSizeInPx = ceil(GridResolutionCm / PixelPerCm);
+    GridCols = ceil(MapWidth * PixelPerCm / GridResolutionCm);
+    GridRows = ceil(MapHeight * PixelPerCm / GridResolutionCm);
+    
+    RegGrid.resize(GridCols);
+    
+    for (int i = 0; i < GridCols; i++){
+        RegGrid[i].resize(GridRows);
+    }
+    for (unsigned i = 0; i < GridCols; ++i) {
+        for (unsigned j = 0; j < GridRows; ++j) {
+            
+            // defind white px counter
+            unsigned PxWhiteConuter = 0;
+            
+            // find out from the png if cell is black or white
+            for (unsigned pI = 0; pI < GridCellSizeInPx; ++pI) {
+                for (unsigned pJ = 0; pJ < GridCellSizeInPx; ++pJ) {
+                    PxWhiteConuter += PngMap[(i * GridCellSizeInPx * MapWidth
+                                              + pI) * 4 + (j * GridCellSizeInPx + pJ) * 4];
+                }
+            }
+            
+            // Chack for number of black cell TODO:think if i need a parameter for Negligible number of blac px
+            if (PxWhiteConuter == (GridCellSizeInPx * GridCellSizeInPx)) {
+                // white
+                RegGrid[i][j] = 1;
+            } else {
+                // Black
+                RegGrid[i][j] = 0;
+            }
+            
+        }
+        
+    }
+    
+    return RegGrid;
+    
 }
 Map::~Map() {
 }
--- Models/Map.h
+++ Models/Map.h
@@ -14,27 +14,29 @@
 using namespace Managers;
 class Map {
 public:
-	Map(const char* filename);
-	Map(unsigned Rows,unsigned Cols);
-	~Map();
-	unsigned m_Rows;
-	unsigned m_Cols;
-
-	void PrintMap(const char* filename);
-
-	inline unsigned getCols() {
-		return m_Cols;
-	};
-
-	inline unsigned getRows() {
-		return m_Rows;
-	};
+    Map(const char* filename);
+    Map(unsigned Rows,unsigned Cols);
+    ~Map();
+    unsigned m_Rows;
+    unsigned m_Cols;
+    std::vector< std::vector<unsigned char> > FatGrid;
+    std::vector< std::vector<unsigned char> > RegGrid;
+    
+    void PrintMap(const char* filename);
+    
+    inline unsigned getCols() {
+        return m_Cols;
+    };
+    
+    inline unsigned getRows() {
+        return m_Rows;
+    };
 protected:
-
-	std::vector<unsigned char> FatGrid;
-	std::vector<unsigned char> RegGrid;
-
+    unsigned width, height;
+    std::vector<unsigned char> RegImage;
+    std::vector<unsigned char> FatImage;
+    
 private:
-	void initMap(const char* filename);
-	std::vector<unsigned char> CreatGridFromMap(std::vector<unsigned char> PngMap,unsigned MapHeight,unsigned MapWidth, float GridResolutionCm, float PixelPerCm, unsigned &GridCols, unsigned &GridRows);
+    void initMap(const char* filename);
+    std::vector< std::vector<unsigned char> > CreatGridFromMap(std::vector<unsigned char> PngMap,unsigned MapHeight,unsigned MapWidth, float GridResolutionCm, float PixelPerCm, unsigned &GridCols, unsigned &GridRows);
 };
