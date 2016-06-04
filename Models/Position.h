75c857a42178012e91dc4cc2d392224f99f423de
meoril <meorilehr@gmail.com>
2016-06-04 17:51:00 +0300

    adding ifndef to position

--- Models/Position.h
+++ Models/Position.h
@@ -1,32 +1,29 @@
-﻿/*
- * Position.h
- *
- *  Created on: May 28, 2016
- *      Author: colman
- */
+﻿
+#ifndef POSITION_H_
+#define POSITION_H_
 #include "Point.h"
 
 class Position {
 public:
-	Position();
-	Position(double x, double y, double Yaw);
-	virtual ~Position();
-	void setX(double x);
-	void setY(double y);
-	void setYaw(double yaw);
-
-	Point m_Pnt;
-	double m_Yaw;
-	inline unsigned getX() {
-		return m_Pnt.m_X;
-	};
-
-	inline unsigned getY() {
-		return m_Pnt.m_Y;
-	};
-
-	inline unsigned getYaw() {
-		return m_Yaw;
-	};
-
+    Position();
+    Position(double x, double y, double Yaw);
+    virtual ~Position();
+    void setX(double x);
+    void setY(double y);
+    void setYaw(double yaw);
+    
+    Point m_Pnt;
+    double m_Yaw;
+    inline unsigned getX() {
+        return m_Pnt.m_X;
+    };
+    
+    inline unsigned getY() {
+        return m_Pnt.m_Y;
+    };
+    
+    inline unsigned getYaw() {
+        return m_Yaw;
+    };
 };
+#endif
