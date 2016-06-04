4f711dfadd250e6fc580e424d541c01c49eb680d
meoril <meorilehr@gmail.com>
2016-06-04 17:29:00 +0300

    adding the robot to the facade

--- Facade.cpp
+++ Facade.cpp
@@ -8,13 +8,16 @@
 #include "Facade.h"
 
 Facade::Facade() {
-    config = new ConfigurationManager("Resources/Parameters.txt");
-    myMap = new Map(config);
+    myConfig = new ConfigurationManager("Resources/Parameters.txt");
+    myMap = new Map(myConfig);
+    myRobot = new Robot("127.0.0.1", 6555, myConfig->getStartPosition(), myConfig->getGoal(), myConfig->getRobotSize(), true);
+    
 }
 
 Facade::~Facade() {
 }
 
 void Facade::Run(){
-    
+    PathPlanner* pathPlanner = new PathPlanner();
+    //std::list<Node*> lstStartWaypoints = pathPlanner->AStarPath();
 }
--- Facade.h
+++ Facade.h
@@ -8,7 +8,11 @@
 #ifndef FACADE_H_
 #define FACADE_H_
 #include "Models/Map.h"
+#include "Models/Point.h"
+#include "Models/Robot.h"
+#include "Utils/PathPlanner.h"
 #include "Managers/ConfigurationManager.h"
+#include "Models/Robot.h"
 class Facade {
 public:
     Facade();
@@ -16,7 +20,8 @@ public:
     void Run();
 private:
     Map* myMap;
-    ConfigurationManager* config;
+    ConfigurationManager* myConfig;
+    Robot* myRobot;
 };
 
 #endif /* FACADE_H_ */
