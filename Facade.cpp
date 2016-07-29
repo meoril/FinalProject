daeaf34d762d7460798f25d991a6d725ab437067
meoril <meorilehr@gmail.com>
2016-07-29 13:39:06 +0300

    fix print path

--- Facade.cpp
+++ Facade.cpp
@@ -12,20 +12,21 @@ Facade::Facade()
     myConfig = new ConfigurationManager("Resources/parameters.txt");
     myMap = new Map(myConfig);
     myRobot = new Robot("127.0.0.1", 6665, myConfig->getStartPosition(), myConfig->getGoal(), myConfig->getRobotSize(), true);
-
+    
     LaserProxy* lsProxy;
     locManager = new LocalizationManager(myMap, lsProxy, new Position(
-    		myRobot->getX(), myRobot->getY(), myRobot->getYaw()));
-
+                                                                      myRobot->getX(), myRobot->getY(), myRobot->getYaw()));
+    
     moveManager = new MovementManager(myRobot);
 }
 
 Facade::~Facade() {
-	delete myConfig;
-	delete myMap;
-	delete myRobot;
-	delete locManager;
-	delete moveManager;
+    delete myConfig;
+    delete myMap;
+    delete myRobot;
+    delete locManager;
+    delete moveManager;
+    
 }
 
 void Facade::Run()
@@ -33,60 +34,70 @@ void Facade::Run()
     PathPlanner* pathPlanner = new PathPlanner();
     Point* tempStartPoint = new Point(myConfig->getStartPositionInGrid().getX(), myConfig->getStartPositionInGrid().getY());
     std::list<Node*> lstPath = pathPlanner->AStarPath(*tempStartPoint, myConfig->getGoalInGrid(), myMap);
+    this->printPath(lstPath,"before.png");
+    cout << lstPath.size() << endl;
     lstPath = pathPlanner->AStarClearList(lstPath, myConfig->getGoalInGrid());
-    this->printPath(*tempStartPoint, myConfig->getGoalInGrid());
-
+    this->printPath(lstPath, "after.png");
+    
+    cout << lstPath.size() << endl;
     std::list<Node*> lstWayPoints;
     WaypointManager* mngWayPoint = new WaypointManager();
     mngWayPoint->createWayPoints(lstPath, &lstWayPoints);
     Particle* best;
-
-
-
+    
+    
     for (std::list<Node*>::iterator listIterator = lstWayPoints.begin();
-    				listIterator != lstWayPoints.end() ;listIterator++)
+         listIterator != lstWayPoints.end() ;listIterator++)
     {
-    	Node* nCurr = *listIterator;
-    	int nCurrYaw = moveManager->moveToNextWatPoint(((Node*)*listIterator)->getX(),
-    									((Node*)*listIterator)->getY(),
-    									locManager);
-
-		best = locManager->getBestParticle();
-
-		locManager->update(best->getPosition().getX() - nCurr->getX(),
-						   best->getPosition().getY() - nCurr->getY(),
-						   best->getPosition().getYaw() - nCurrYaw);
-
-
-		cout << "Best particle: x - " << best->getPosition().getX() << "  y - " << best->getPosition().getX() << endl
-			 << "Current position: x - " << myRobot->getX() << " y - " << myRobot->getY() << endl << endl;
-
+        Node* nCurr = *listIterator;
+        int nCurrYaw = moveManager->moveToNextWatPoint(((Node*)*listIterator)->getX(),
+                                                       ((Node*)*listIterator)->getY(),
+                                                       locManager);
+        
+        best = locManager->getBestParticle();
+        
+        locManager->update(best->getPosition().getX() - nCurr->getX(),
+                           best->getPosition().getY() - nCurr->getY(),
+                           best->getPosition().getYaw() - nCurrYaw);
+        
+        
+        cout << "Best particle: x - " << best->getPosition().getX() << "  y - " << best->getPosition().getX() << endl
+    			 << "Current position: x - " << myRobot->getX() << " y - " << myRobot->getY() << endl << endl;
+        
     }
 }
 
-void Facade::printPath(Point startPoint, Point endPoint){
-    // Start location
-    myMap->FatImage[(myConfig->getStartPositionInGrid().getX() * 4 + myConfig->getStartPositionInGrid().getY() * myMap->width * 4) + 0] = 255;
-    myMap->FatImage[(myConfig->getStartPositionInGrid().getX() * 4 + myConfig->getStartPositionInGrid().getY() * myMap->width * 4) + 1] = 0;
-    myMap->FatImage[(myConfig->getStartPositionInGrid().getX() * 4 + myConfig->getStartPositionInGrid().getY() * myMap->width * 4) + 2] = 0;
-    myMap->FatImage[(myConfig->getStartPositionInGrid().getX() * 4 + myConfig->getStartPositionInGrid().getY() * myMap->width * 4) + 3] = 0.1;
+void Facade::printPath(std::list<Node*>& lstStarPath, std::string name){
     
+    Position startPoint = myConfig->getStartPositionInGrid();
+    Position bla = myConfig->getStartPosition();
+    int nStartPoint = (myConfig->getStartPositionInGrid().getX() * 4 + myConfig->getStartPositionInGrid().getY() * myMap->width * 4);
+    // Start location
+    myMap->FatImage[nStartPoint + 0] = 255;//255
+    myMap->FatImage[nStartPoint + 1] = 0;
+    myMap->FatImage[nStartPoint + 2] = 0;
+    myMap->FatImage[nStartPoint + 3] = 0.1;
+    
+    Point goalPoint = myConfig->getGoalInGrid();
+    Point stam = myConfig->getGoal();
+    int nGoalPoint = (myConfig->getGoalInGrid().getX() * 4 + myConfig->getGoalInGrid().getY() * myMap->width * 4);
     // Goal location
-    myMap->FatImage[(myConfig->getGoalInGrid().getX() * 4 + myConfig->getGoalInGrid().getY() * myMap->width * 4) + 0] = 0;
-    myMap->FatImage[(myConfig->getGoalInGrid().getX() * 4 + myConfig->getGoalInGrid().getY() * myMap->width * 4) + 1] = 255;
-    myMap->FatImage[(myConfig->getGoalInGrid().getX() * 4 + myConfig->getGoalInGrid().getY() * myMap->width * 4) + 2] = 0;
-    myMap->FatImage[(myConfig->getGoalInGrid().getX() * 4 + myConfig->getGoalInGrid().getY() * myMap->width * 4) + 3] = 0.1;
+    myMap->FatImage[nGoalPoint + 0] = 0;
+    myMap->FatImage[nGoalPoint + 1] = 255;//255
+    myMap->FatImage[nGoalPoint + 2] = 0;
+    myMap->FatImage[nGoalPoint + 3] = 0.1;
     
     // AStar
-    for (std::list<Node*>::iterator listIterator = lstClearWaypointToDestination.begin(); listIterator != lstClearWaypointToDestination.end(); listIterator++)
+    for (std::list<Node*>::iterator listIterator = lstStarPath.begin(); listIterator != lstStarPath.end(); listIterator++)
     {
         int nX = (*listIterator)->getX() * myConfig->getGridMapResolution();
         int nY = (*listIterator)->getY() * myConfig->getGridMapResolution();
-        myMap->FatImage[(nX * 4 + nY * myMap->width * 4) + 0] = 0;
-        myMap->FatImage[(nX * 4 + nY * myMap->width * 4) + 1] = 255;
-        myMap->FatImage[(nX * 4 + nY * myMap->width * 4) + 2] = 0;
-        myMap->FatImage[(nX * 4 + nY * myMap->width * 4) + 3] = 0.1;
+        int nPoint = (nX * 4 + nY * myMap->width * 4);
+        myMap->FatImage[nPoint + 0] = 0;
+        myMap->FatImage[nPoint + 1] = 255;//255
+        myMap->FatImage[nPoint + 2] = 0;
+        myMap->FatImage[nPoint + 3] = 0.1;
     }
     
-    lodepng::encode("AStar.png", myMap->FatImage, myMap->width, myMap->height);
+    lodepng::encode(name, myMap->FatImage, myMap->width, myMap->height);
 }
