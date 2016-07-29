899cfe5b99f13dd1b86573ff8b2fa1c0fa48fcd5
meoril <meorilehr@gmail.com>
2016-07-29 10:56:16 +0300

    even better

--- Models/Map.cpp
+++ Models/Map.cpp
@@ -120,23 +120,23 @@ std::vector< std::vector<unsigned char> > Map::CreatGridFromMap(std::vector<unsi
         tempGrid[i].resize(GridRows);
     }
     
-    fstream file;
-    file.open("meoriTry.txt");
+    //fstream file;
+    //file.open("meoriTry.txt");
     
     for (unsigned i = 0; i < GridCols; ++i) {
         for (unsigned j = 0; j < GridRows; ++j) {
             tempGrid[i][j] = this->checkCellOccupation(PngMap, i, j);
-            if (i != 362 && j != 305)
-            file << this->checkCellOccupation(PngMap, i, j);
-            else if (i == 362 && j == 305)
-            file << 8;
-            else
-            file << 5;
+            //if (i != 362 && j != 305)
+            //file << this->checkCellOccupation(PngMap, i, j);
+            //else if (i == 362 && j == 305)
+            //file << 8;
+            //else
+            //file << 5;
         }
-        file << endl;
+        //file << endl;
     }
     
-    file.close();
+    //file.close();
     return tempGrid;
     
 }
